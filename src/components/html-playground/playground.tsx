4d5ef2319fb7a5ab8b92ecdd693a55b350dac82d
Cory LaViska <cory@abeautifulsite.net>
2020-11-05 11:01:25 -0500

    Render preview in light DOM

--- src/components/html-playground/playground.tsx
+++ src/components/html-playground/playground.tsx
@@ -1,5 +1,5 @@
 import { Component, Element, Prop, h } from '@stencil/core';
-import { whiskers } from '../../utils/whiskers';
+import { whiskers } from '../../utils/whiskers/whiskers';
 
 export interface PlaygroundControl {
   name: string;
@@ -62,63 +62,64 @@ export class Playground {
   }
 
   render() {
+    // Inject the preview into the default slot
+    this.host.innerHTML = this.renderPreview();
+
     return (
       <div class="playground">
-        <div class="playground__main">
-          <div class="playground__preview" innerHTML={this.renderPreview()}>
-            <slot />
-          </div>
-          <div class="playground__controls">
-            {this.controls.map(control => (
-              <div class={`playground__control playground__control--${control.type}`}>
-                <label>
-                  <span class="playground__control-name">{this.camelToSpace(control.name)}</span>
+        <div class="playground__preview">
+          <slot />
+        </div>
+        <div class="playground__controls">
+          {this.controls.map(control => (
+            <div class={`playground__control playground__control--${control.type}`}>
+              <label>
+                <span class="playground__control-name">{this.camelToSpace(control.name)}</span>
 
-                  {control.type === 'text' && (
-                    <input type="text" name={control.name} value={control.value} onInput={event => this.setControlValue(control.name, (event.target as HTMLInputElement).value)} />
-                  )}
+                {control.type === 'text' && (
+                  <input type="text" name={control.name} value={control.value} onInput={event => this.setControlValue(control.name, (event.target as HTMLInputElement).value)} />
+                )}
 
-                  {control.type === 'color' && (
-                    <input type="color" name={control.name} value={control.value} onInput={event => this.setControlValue(control.name, (event.target as HTMLInputElement).value)} />
-                  )}
+                {control.type === 'color' && (
+                  <input type="color" name={control.name} value={control.value} onInput={event => this.setControlValue(control.name, (event.target as HTMLInputElement).value)} />
+                )}
 
-                  {control.type === 'boolean' && (
-                    <input
-                      type="checkbox"
-                      name={control.name}
-                      checked={control.value + '' === 'true'}
-                      onClick={event => this.setControlValue(control.name, (event.target as HTMLInputElement).checked)}
-                    />
-                  )}
+                {control.type === 'boolean' && (
+                  <input
+                    type="checkbox"
+                    name={control.name}
+                    checked={control.value + '' === 'true'}
+                    onClick={event => this.setControlValue(control.name, (event.target as HTMLInputElement).checked)}
+                  />
+                )}
 
-                  {control.type === 'range' && (
-                    <input
-                      type="range"
-                      name={control.name}
-                      min={control.min}
-                      max={control.max}
-                      step={control.step}
-                      value={control.value}
-                      onInput={event => this.setControlValue(control.name, (event.target as HTMLInputElement).value)}
-                    />
-                  )}
+                {control.type === 'range' && (
+                  <input
+                    type="range"
+                    name={control.name}
+                    min={control.min}
+                    max={control.max}
+                    step={control.step}
+                    value={control.value}
+                    onInput={event => this.setControlValue(control.name, (event.target as HTMLInputElement).value)}
+                  />
+                )}
 
-                  {control.type === 'select' && (
-                    <select
-                      name={control.name}
-                      onInput={event => this.setControlValue(control.name, (event.target as HTMLSelectElement).options[(event.target as HTMLSelectElement).selectedIndex].value)}
-                    >
-                      {control.options.map(value => (
-                        <option value={value} selected={value === control.value}>
-                          {value}
-                        </option>
-                      ))}
-                    </select>
-                  )}
-                </label>
-              </div>
-            ))}
-          </div>
+                {control.type === 'select' && (
+                  <select
+                    name={control.name}
+                    onInput={event => this.setControlValue(control.name, (event.target as HTMLSelectElement).options[(event.target as HTMLSelectElement).selectedIndex].value)}
+                  >
+                    {control.options.map(value => (
+                      <option value={value} selected={value === control.value}>
+                        {value}
+                      </option>
+                    ))}
+                  </select>
+                )}
+              </label>
+            </div>
+          ))}
         </div>
       </div>
     );
